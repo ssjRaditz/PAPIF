c8bbb05eb188b3bf20075dd8c4fc3e533db43808
ssjRaditz <tom-herrick@hotmail.co.uk>
2026-06-17 09:17:37 +0100

    Tidy for deployment

---
+++ pests_data.js
@@ -0,0 +1,102 @@
+window.PESTS_DATA = [
+  {
+    "id": 1,
+    "name": "Black bean aphid"
+  },
+  {
+    "id": 2,
+    "name": "Cabbage white (butterfly and larva)"
+  },
+  {
+    "id": 3,
+    "name": "Capsid bug"
+  },
+  {
+    "id": 4,
+    "name": "Chafer grub"
+  },
+  {
+    "id": 5,
+    "name": "Flea beetle (Phyllotreta)"
+  },
+  {
+    "id": 6,
+    "name": "Frog hopper"
+  },
+  {
+    "id": 7,
+    "name": "Glasshouse red spider mite"
+  },
+  {
+    "id": 8,
+    "name": "Horse chestnut leaf miner"
+  },
+  {
+    "id": 9,
+    "name": "Lily beetle"
+  },
+  {
+    "id": 10,
+    "name": "Mealybug"
+  },
+  {
+    "id": 11,
+    "name": "Scale insect"
+  },
+  {
+    "id": 12,
+    "name": "Sciarid fly"
+  },
+  {
+    "id": 13,
+    "name": "Slug"
+  },
+  {
+    "id": 14,
+    "name": "Vine weevil (adult and larvae)"
+  },
+  {
+    "id": 15,
+    "name": "Whitefly (brassica and glasshouse)"
+  },
+  {
+    "id": 16,
+    "name": "Black spot"
+  },
+  {
+    "id": 17,
+    "name": "Botrytis"
+  },
+  {
+    "id": 18,
+    "name": "Box blight"
+  },
+  {
+    "id": 19,
+    "name": "Bacterial canker"
+  },
+  {
+    "id": 20,
+    "name": "Damping off"
+  },
+  {
+    "id": 21,
+    "name": "Downy mildew"
+  },
+  {
+    "id": 22,
+    "name": "Honey fungus"
+  },
+  {
+    "id": 23,
+    "name": "Pear rust"
+  },
+  {
+    "id": 24,
+    "name": "Powdery mildew"
+  },
+  {
+    "id": 25,
+    "name": "Fascination"
+  }
+];
---
+++ plants_data.js
@@ -0,0 +1,442 @@
+window.PLANTS_DATA = [
+  {
+    "id": 1,
+    "name": "X Cuprocyparis leylandii"
+  },
+  {
+    "id": 2,
+    "name": "Abelia x grandiflora"
+  },
+  {
+    "id": 3,
+    "name": "Acanthus mollis"
+  },
+  {
+    "id": 4,
+    "name": "Acer griseum"
+  },
+  {
+    "id": 5,
+    "name": "Acer palmatum"
+  },
+  {
+    "id": 6,
+    "name": "Achillea millefolium"
+  },
+  {
+    "id": 7,
+    "name": "Aegopodium podagraria"
+  },
+  {
+    "id": 8,
+    "name": "Aesculus hippocastanum"
+  },
+  {
+    "id": 9,
+    "name": "Agave americana"
+  },
+  {
+    "id": 10,
+    "name": "Alchemilla mollis"
+  },
+  {
+    "id": 11,
+    "name": "Araucaria araucana"
+  },
+  {
+    "id": 12,
+    "name": "Arbutus unedo"
+  },
+  {
+    "id": 13,
+    "name": "Armeria maritima"
+  },
+  {
+    "id": 14,
+    "name": "Asplenium scolopendrium"
+  },
+  {
+    "id": 15,
+    "name": "Astrantia major cv."
+  },
+  {
+    "id": 16,
+    "name": "Bellis perennis"
+  },
+  {
+    "id": 17,
+    "name": "Berberis darwinii"
+  },
+  {
+    "id": 18,
+    "name": "Betula pendula"
+  },
+  {
+    "id": 19,
+    "name": "Buddleja davidii"
+  },
+  {
+    "id": 20,
+    "name": "Buxus sempervirens"
+  },
+  {
+    "id": 21,
+    "name": "Caltha palustris"
+  },
+  {
+    "id": 22,
+    "name": "Calystegia sepium"
+  },
+  {
+    "id": 23,
+    "name": "Campsis radicans cv."
+  },
+  {
+    "id": 24,
+    "name": "Canna indica cv."
+  },
+  {
+    "id": 25,
+    "name": "Capsella bursa-pastoris"
+  },
+  {
+    "id": 26,
+    "name": "Centaurea cyanus"
+  },
+  {
+    "id": 27,
+    "name": "Choisya ternata 'Sundance'"
+  },
+  {
+    "id": 28,
+    "name": "Clematis armandi"
+  },
+  {
+    "id": 29,
+    "name": "Clivia miniata"
+  },
+  {
+    "id": 30,
+    "name": "Cordyline australis"
+  },
+  {
+    "id": 31,
+    "name": "Cornus sanguinea cv."
+  },
+  {
+    "id": 32,
+    "name": "Corydalis flexuosa"
+  },
+  {
+    "id": 33,
+    "name": "Corylus avellana"
+  },
+  {
+    "id": 34,
+    "name": "Cosmos bipinnatus"
+  },
+  {
+    "id": 35,
+    "name": "Cotinus coggygria"
+  },
+  {
+    "id": 36,
+    "name": "Crataegus monogyna"
+  },
+  {
+    "id": 37,
+    "name": "Cyclamen hederifolium"
+  },
+  {
+    "id": 38,
+    "name": "Dahlia \u2018Bishop of Llandaff\u2019"
+  },
+  {
+    "id": 39,
+    "name": "Daphne bholua"
+  },
+  {
+    "id": 40,
+    "name": "Daucus carota"
+  },
+  {
+    "id": 41,
+    "name": "Echinacea purpurea cv."
+  },
+  {
+    "id": 42,
+    "name": "Ceratophyllum demersum"
+  },
+  {
+    "id": 43,
+    "name": "Elymus repens"
+  },
+  {
+    "id": 44,
+    "name": "Eryngium giganteum"
+  },
+  {
+    "id": 45,
+    "name": "Euphorbia characias subsp. wulfenii"
+  },
+  {
+    "id": 46,
+    "name": "Fagus sylvatica"
+  },
+  {
+    "id": 47,
+    "name": "Fatsia japonica"
+  },
+  {
+    "id": 48,
+    "name": "Forsythia x intermedia"
+  },
+  {
+    "id": 49,
+    "name": "Fritillaria meleagris"
+  },
+  {
+    "id": 50,
+    "name": "Ginkgo biloba"
+  },
+  {
+    "id": 51,
+    "name": "Griselinia littoralis"
+  },
+  {
+    "id": 52,
+    "name": "Hamamelis mollis cv."
+  },
+  {
+    "id": 53,
+    "name": "Hedera helix cv."
+  },
+  {
+    "id": 54,
+    "name": "Heliotropium arborescens"
+  },
+  {
+    "id": 55,
+    "name": "Helleborus x hybridus cv."
+  },
+  {
+    "id": 56,
+    "name": "Ilex aquifolium"
+  },
+  {
+    "id": 57,
+    "name": "Iris pseudacorus"
+  },
+  {
+    "id": 58,
+    "name": "Lavandula angustifolia"
+  },
+  {
+    "id": 59,
+    "name": "Leucanthemum vulgare"
+  },
+  {
+    "id": 60,
+    "name": "Lewisia cotyledon"
+  },
+  {
+    "id": 61,
+    "name": "Ligustrum ovalifolium"
+  },
+  {
+    "id": 62,
+    "name": "Liriope muscari"
+  },
+  {
+    "id": 63,
+    "name": "Lobelia cardinalis"
+  },
+  {
+    "id": 64,
+    "name": "Lonicera periclymenum cv."
+  },
+  {
+    "id": 65,
+    "name": "Magnolia stellata"
+  },
+  {
+    "id": 66,
+    "name": "Matteuccia struthiopteris"
+  },
+  {
+    "id": 67,
+    "name": "Metasequoia glyptostroboides"
+  },
+  {
+    "id": 68,
+    "name": "Miscanthus sinensis cv."
+  },
+  {
+    "id": 69,
+    "name": "Monstera deliciosa"
+  },
+  {
+    "id": 70,
+    "name": "Muscari armeniacum"
+  },
+  {
+    "id": 71,
+    "name": "Nigella damascena"
+  },
+  {
+    "id": 72,
+    "name": "Oenothera biennis"
+  },
+  {
+    "id": 73,
+    "name": "Ophiopogon planiscapus \u2018Kokury\u016b\u2019"
+  },
+  {
+    "id": 74,
+    "name": "Parthenocissus quinquefolia"
+  },
+  {
+    "id": 75,
+    "name": "Phlomis fruticosa"
+  },
+  {
+    "id": 76,
+    "name": "Phlox paniculata cv."
+  },
+  {
+    "id": 77,
+    "name": "Phormium tenax"
+  },
+  {
+    "id": 78,
+    "name": "Photinia x fraseri \u2018Red Robin\u2019"
+  },
+  {
+    "id": 79,
+    "name": "Phyllostachys aurea"
+  },
+  {
+    "id": 80,
+    "name": "Plantago major"
+  },
+  {
+    "id": 81,
+    "name": "Pontederia cordata"
+  },
+  {
+    "id": 82,
+    "name": "Primula vulgaris"
+  },
+  {
+    "id": 83,
+    "name": "Prunella vulgaris"
+  },
+  {
+    "id": 84,
+    "name": "Prunus serrula"
+  },
+  {
+    "id": 85,
+    "name": "Pulsatilla vulgaris"
+  },
+  {
+    "id": 86,
+    "name": "Quercus robur"
+  },
+  {
+    "id": 87,
+    "name": "Ranunculus repens"
+  },
+  {
+    "id": 88,
+    "name": "Rhinanthus minor"
+  },
+  {
+    "id": 89,
+    "name": "Rudbeckia fulgida var. deamii"
+  },
+  {
+    "id": 90,
+    "name": "Salvia rosmarinus"
+  },
+  {
+    "id": 91,
+    "name": "Salvia nemorosa cv."
+  },
+  {
+    "id": 92,
+    "name": "Sansevieria trifasciata cv."
+  },
+  {
+    "id": 93,
+    "name": "Sarcococca confusa"
+  },
+  {
+    "id": 94,
+    "name": "Sempervivum arachnoideum"
+  },
+  {
+    "id": 95,
+    "name": "Skimmia japonica"
+  },
+  {
+    "id": 96,
+    "name": "Spathiphyllum wallisii"
+  },
+  {
+    "id": 97,
+    "name": "Stellaria media"
+  },
+  {
+    "id": 98,
+    "name": "Stipa gigantea"
+  },
+  {
+    "id": 99,
+    "name": "Symphyotrichum novi-belgii cv."
+  },
+  {
+    "id": 100,
+    "name": "Tagetes erecta"
+  },
+  {
+    "id": 101,
+    "name": "Taraxacum officinale"
+  },
+  {
+    "id": 102,
+    "name": "Taxus baccata"
+  },
+  {
+    "id": 103,
+    "name": "Trachelospermum jasminoides"
+  },
+  {
+    "id": 104,
+    "name": "Typha angustifolia"
+  },
+  {
+    "id": 105,
+    "name": "Urtica dioica"
+  },
+  {
+    "id": 106,
+    "name": "Verbena bonariensis"
+  },
+  {
+    "id": 107,
+    "name": "Viburnum davidii"
+  },
+  {
+    "id": 108,
+    "name": "Vinca major cv."
+  },
+  {
+    "id": 109,
+    "name": "Wisteria sinensis"
+  },
+  {
+    "id": 110,
+    "name": "Zamioculcas zamiifolia"
+  }
+];
